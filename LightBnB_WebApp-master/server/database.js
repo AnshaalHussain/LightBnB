6595d7b1ad88e2b32dbe5e51a9230e4c1b86a65b
Anshaal Hussain <anshaalhussain@gmail.com>
2021-10-16 06:11:16 -0400

    getallproperties function takes in user parameters and queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -113,7 +113,34 @@ exports.getAllReservations = getAllReservations;
 
 exports.getAllProperties = getAllProperties;
 
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
 
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
